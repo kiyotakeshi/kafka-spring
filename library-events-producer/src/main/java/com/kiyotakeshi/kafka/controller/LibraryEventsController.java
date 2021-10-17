a831bb0761bf092fdae91dac2b271f07bad43662
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-10-17 23:56:55 +0900

    add synchronous method

--- library-events-producer/src/main/java/com/kiyotakeshi/kafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/kiyotakeshi/kafka/controller/LibraryEventsController.java
@@ -3,15 +3,21 @@ package com.kiyotakeshi.kafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kiyotakeshi.kafka.domain.LibraryEvent;
 import com.kiyotakeshi.kafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @RestController
 @RequestMapping("/v1")
+@Slf4j
 public class LibraryEventsController {
 
     private LibraryEventProducer libraryEventProducer;
@@ -21,10 +27,14 @@ public class LibraryEventsController {
     }
 
     @PostMapping("/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 
         // invoke kafka producer
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("before send libraryevent");
+        // libraryEventProducer.sendLibraryEvent(libraryEvent);
+        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("send result is {}", sendResult.toString());
+        log.info("after send libraryevent");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
--- library-events-producer/src/main/java/com/kiyotakeshi/kafka/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/kiyotakeshi/kafka/producer/LibraryEventProducer.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kiyotakeshi.kafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Component
 @Slf4j
@@ -39,6 +42,23 @@ public class LibraryEventProducer {
                 handleSuccess(key, value, result);
             }
         });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        SendResult<Integer, String> sendResult = null;
+        try {
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("ExecutionException | InterruptedException sending the message and  the exception is {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception sending the message and  the exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
     }
 
     private void handleFailure(Integer key, String value, Throwable ex) {
