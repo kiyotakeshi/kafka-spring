53b35d13066f6dfd9f88d2b4579023157164160b
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-12-12 17:21:52 +0900

    set multiple broker

--- library-events-producer/src/main/java/com/kiyotakeshi/kafka/config/AutoCreateConfig.java
+++ library-events-producer/src/main/java/com/kiyotakeshi/kafka/config/AutoCreateConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-@Profile("local") // invoke only for the local profile
 public class AutoCreateConfig {
 
     @Bean
