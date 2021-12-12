b8866e8e741e1f1334f35862a822d710c408d83e
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-12-13 08:45:02 +0900

    set concurrency

--- library-events-consumer/src/main/java/com/kiyotakeshi/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/kiyotakeshi/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -21,7 +21,12 @@ public class LibraryEventsConsumerConfig {
 //        configurer.configure(factory, kafkaConsumerFactory);
 //        // @see https://docs.spring.io/spring-kafka/reference/html/#committing-offsets
 //        // @see https://spring.pleiades.io/spring-kafka/docs/current/reference/html/#ooo-commits
-//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//        // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//
+//        // factory.setConcurrency(3);
+//        // 2021-12-13 08:43:05.096  INFO 35878 --- [ntainer#0-0-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-group: partitions assigned: [library-events-0]
+//        // 2021-12-13 08:43:05.096  INFO 35878 --- [ntainer#0-1-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-group: partitions assigned: [library-events-1]
+//        // 2021-12-13 08:43:05.096  INFO 35878 --- [ntainer#0-2-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-group: partitions assigned: [library-events-2]
 //        return factory;
 //    }
 }
