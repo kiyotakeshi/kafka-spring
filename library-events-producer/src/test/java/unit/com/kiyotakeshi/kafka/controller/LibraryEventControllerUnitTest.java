313aacf91c57db4d860faffa0d3cfa1da0341598
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-12-12 21:14:01 +0900

    add web layer slice test

---
+++ library-events-producer/src/test/java/unit/com/kiyotakeshi/kafka/controller/LibraryEventControllerUnitTest.java
@@ -0,0 +1,58 @@
+package com.kiyotakeshi.kafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kiyotakeshi.kafka.domain.Book;
+import com.kiyotakeshi.kafka.domain.LibraryEvent;
+import com.kiyotakeshi.kafka.producer.LibraryEventProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LibraryEventsController.class)
+@AutoConfigureMockMvc
+public class LibraryEventControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    LibraryEventProducer libraryEventProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+        // given
+        var book = Book.builder()
+                .bookId(12)
+                .bookAuthor("Taro")
+                .bookName("Kafka with Spring Boot")
+                .build();
+
+        var libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        // when
+        mockMvc.perform(post("/v1/libraryevent")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        // then
+    }
+}
