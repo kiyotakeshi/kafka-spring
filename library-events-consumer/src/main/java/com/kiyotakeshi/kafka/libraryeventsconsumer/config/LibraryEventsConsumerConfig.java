0bebb94157f35818f366d101798393649122f3df
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-12-13 08:41:09 +0900

    manual offset operation

--- library-events-consumer/src/main/java/com/kiyotakeshi/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/com/kiyotakeshi/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,27 @@
 package com.kiyotakeshi.kafka.libraryeventsconsumer.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+//    // copied by org/springframework/boot/autoconfigure/kafka/KafkaAnnotationDrivenConfiguration.java#kafkaListenerContainerFactory
+//    @Bean
+//    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+//            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+//            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+//        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        configurer.configure(factory, kafkaConsumerFactory);
+//        // @see https://docs.spring.io/spring-kafka/reference/html/#committing-offsets
+//        // @see https://spring.pleiades.io/spring-kafka/docs/current/reference/html/#ooo-commits
+//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//        return factory;
+//    }
 }
